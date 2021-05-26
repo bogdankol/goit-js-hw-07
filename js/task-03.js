b98c7164c2b88220026f1e35506796c271833007
bogdankol <bogdank208@gmail.com>
2021-05-26 16:27:23 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -8,39 +8,42 @@
 
 const images = [
   {
-    url:
-      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url:
-      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const galleryRef = document.querySelector(`#gallery`)
+const galleryRef = document.querySelector(`#gallery`);
 galleryRef.classList.add(`js-gallery`);
 
-const lis = images.map(({ url, alt }) => {
-  const liEl = document.createElement(`li`);
-  liEl.insertAdjacentHTML(
-    `beforeend`,
-    `<img src=${url} alt=${alt}></img>`
-  );
-  return liEl;
-})
-galleryRef.append(...lis)
+// 1 - string - we insert stringed all li-s as one innerHTML string - html transforms it into
+// markup automatically
+const toDoListMarkUp = function ({ url, alt }) {
+  return `<li><img src=${url} alt=${alt}></li>`
+}
+const arrayToString = images.map(element => toDoListMarkUp(element)).join(``)
+galleryRef.insertAdjacentHTML(`afterbegin`, arrayToString)
 
 
 
-
-
+// 2
+// const lis = images.map(({ url, alt }) => {
+//   const liEl = document.createElement(`li`);
+//   liEl.insertAdjacentHTML(
+//     `beforeend`,
+//     `<img src=${url} alt=${alt}></img>`
+//   );
+//   return liEl;
+// })
+// galleryRef.append(...lis)
 
 // другой способ
 // images.forEach(({url, alt}) => {
@@ -50,7 +53,7 @@ galleryRef.append(...lis)
 //     imgEl.setAttribute(`src`, url);
 //     imgEl.setAttribute(`alt`, alt);
 //     liEl.appendChild(imgEl);
-//     galleryRef.appendChild(liEl); 
+//     galleryRef.appendChild(liEl);
 // })
 
 //третий способ - оптимизация
@@ -60,12 +63,3 @@ galleryRef.append(...lis)
 //     `<li><img src=${url} alt=${alt}></img></li>`
 //   );
 // });
-
-
-
-
-
-
-
-
-
